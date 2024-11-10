5a1675060a401a744863f44ac9de5fda4b75157f
zanmlakar123 <zanmlakar58@gmail.com>
2024-11-10 11:23:39 +0100

    rok zabcic

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Rok zabcic 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Rok zabcic
+          
         </a>
       </header>
     </div>
